021c4befa49f31f28d333a616f91f1fd0146c009
moximoxiknight <43071812+moximoxiknight@users.noreply.github.com>
2018-11-29 22:54:48 +0800

    Update goodies.h

--- src/mongo/util/goodies.h
+++ src/mongo/util/goodies.h
@@ -1,21 +1,6 @@
 // @file goodies.h
 // miscellaneous
 
-/*    Copyright 2009 10gen Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 #pragma once
 
 #include <boost/detail/endian.hpp>
@@ -23,6 +8,9 @@
 
 #include "mongo/bson/util/misc.h"
 #include "mongo/util/concurrency/mutex.h"
+
+//Knight : add verify
+#define verify mongo::MONGO_verify
 
 namespace mongo {
 
