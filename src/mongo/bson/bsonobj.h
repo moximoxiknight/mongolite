610f25a20d021bcb981330fc30a6fd87173d37e8
moximoxiknight <43071812+moximoxiknight@users.noreply.github.com>
2018-11-29 23:01:03 +0800

    Update bsonobj.h

--- src/mongo/bson/bsonobj.h
+++ src/mongo/bson/bsonobj.h
@@ -1,19 +1,5 @@
 // @file bsonobj.h
 
-/*    Copyright 2009 10gen Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 
 #pragma once
 
@@ -28,6 +14,10 @@
 #include "mongo/bson/stringdata.h"
 #include "mongo/bson/util/atomic_int.h"
 #include "mongo/bson/util/builder.h"
+
+
+//Knight : add verify
+#define verify mongo::MONGO_verify
 
 namespace mongo {
 
